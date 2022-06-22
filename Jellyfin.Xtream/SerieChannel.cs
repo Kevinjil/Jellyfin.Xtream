54592b347527a5a1f07aea77841f61dced87f80f
Kevin Jilissen <info@kevinjilissen.nl>
2022-06-22 20:56:55 +0200

    Fix season loading and use metadata of series and seasons.
    Due to the set FolderType, Jellyfin did not show any season content in the series.

--- Jellyfin.Xtream/SerieChannel.cs
+++ Jellyfin.Xtream/SerieChannel.cs
@@ -128,6 +128,19 @@ namespace Jellyfin.Xtream
             return await GetEpisodes(seriesId, season, cancellationToken).ConfigureAwait(false);
         }
 
+        private List<string> GetGenres(string genreString)
+        {
+            return new List<string>(genreString.Split(',').Select(genre => genre.Trim()));
+        }
+
+        private List<PersonInfo> GetPeople(string cast)
+        {
+            return cast.Split(',').Select(name => new PersonInfo()
+            {
+                Name = name.Trim()
+            }).ToList();
+        }
+
         private async Task<ChannelItemResult> GetCategories(CancellationToken cancellationToken)
         {
             string key = "xtream-series-categories";
@@ -193,19 +206,16 @@ namespace Jellyfin.Xtream
                     ParsedName parsedName = plugin.StreamService.ParseName(serie.Name);
                     items.Add(new ChannelItemInfo()
                     {
+                        CommunityRating = (float)serie.Rating5Based,
+                        DateModified = serie.LastModified,
+                        // FolderType = ChannelFolderType.Series,
+                        Genres = GetGenres(serie.Genre),
                         Id = $"ser-{serie.SeriesId}",
+                        ImageUrl = serie.Cover,
                         Name = parsedName.Title,
+                        People = GetPeople(serie.Cast),
                         Tags = new List<string>(parsedName.Tags),
                         Type = ChannelItemType.Folder,
-                        ImageUrl = serie.Cover,
-                        DateModified = serie.LastModified,
-                        CommunityRating = (float)serie.Rating5Based,
-                        Genres = new List<string>() { serie.Genre },
-                        People = serie.Cast.Split(",").Select(name => new PersonInfo()
-                        {
-                            Name = name.Trim()
-                        }).ToList(),
-                        // FolderType = ChannelFolderType.Series,
                     });
                 }
 
@@ -236,36 +246,44 @@ namespace Jellyfin.Xtream
             using (XtreamClient client = new XtreamClient())
             {
                 SeriesStreamInfo series = await client.GetSeriesStreamsBySeriesAsync(plugin.Creds, seriesId, cancellationToken).ConfigureAwait(false);
+                Series serie = series.Info;
                 List<ChannelItemInfo> items = new List<ChannelItemInfo>();
 
                 foreach (int seasonId in series.Episodes.Keys)
                 {
+                    string name = $"Season {seasonId}";
+                    string cover = series.Info.Cover;
+                    string? overview = null;
+                    DateTime? created = null;
+                    List<string> tags = new List<string>();
+
                     Season? season = series.Seasons.FirstOrDefault(s => s.SeasonId == seasonId);
                     if (season != null)
                     {
                         ParsedName parsedName = plugin.StreamService.ParseName(season.Name);
-                        items.Add(new ChannelItemInfo()
+                        name = parsedName.Title;
+                        tags.AddRange(parsedName.Tags);
+                        created = season.AirDate;
+                        overview = season.Overview;
+                        if (!string.IsNullOrEmpty(season.Cover))
                         {
-                            Id = $"{seriesId}-{seasonId}",
-                            Name = parsedName.Title,
-                            Tags = new List<string>(parsedName.Tags),
-                            Type = ChannelItemType.Folder,
-                            ImageUrl = season.Cover,
-                            DateCreated = season.AirDate,
-                            // FolderType = ChannelFolderType.Season,
-                        });
+                            cover = season.Cover;
+                        }
                     }
-                    else
+
+                    items.Add(new ChannelItemInfo()
                     {
-                        items.Add(new ChannelItemInfo()
-                        {
-                            Id = $"{seriesId}-{seasonId}",
-                            Name = $"Season {seasonId}",
-                            Type = ChannelItemType.Folder,
-                            ImageUrl = series.Info.Cover,
-                            // FolderType = ChannelFolderType.Season,
-                        });
-                    }
+                        DateCreated = created,
+                        // FolderType = ChannelFolderType.Season,
+                        Genres = GetGenres(serie.Genre),
+                        Id = $"{seriesId}-{seasonId}",
+                        ImageUrl = cover,
+                        Name = name,
+                        Overview = overview,
+                        People = GetPeople(serie.Cast),
+                        Tags = tags,
+                        Type = ChannelItemType.Folder,
+                    });
                 }
 
                 ChannelItemResult result = new ChannelItemResult()
@@ -294,10 +312,12 @@ namespace Jellyfin.Xtream
 
             using (XtreamClient client = new XtreamClient())
             {
-                SeriesStreamInfo channel = await client.GetSeriesStreamsBySeriesAsync(plugin.Creds, seriesId, cancellationToken).ConfigureAwait(false);
+                SeriesStreamInfo series = await client.GetSeriesStreamsBySeriesAsync(plugin.Creds, seriesId, cancellationToken).ConfigureAwait(false);
+                Series serie = series.Info;
+                Season? seasonInfo = series.Seasons.FirstOrDefault(s => s.SeasonId == season);
                 List<ChannelItemInfo> items = new List<ChannelItemInfo>();
 
-                foreach (Episode episode in channel.Episodes[season])
+                foreach (Episode episode in series.Episodes[season])
                 {
                     string id = episode.EpisodeId.ToString(System.Globalization.CultureInfo.InvariantCulture);
                     ParsedName parsedName = plugin.StreamService.ParseName(episode.Title);
@@ -306,16 +326,30 @@ namespace Jellyfin.Xtream
                         plugin.StreamService.GetMediaSourceInfo(StreamType.Series, id, episode.ContainerExtension)
                     };
 
+                    string cover = episode.Info.MovieImage;
+                    if (string.IsNullOrEmpty(cover) && seasonInfo != null)
+                    {
+                        cover = seasonInfo.Cover;
+                    }
+
+                    if (string.IsNullOrEmpty(cover))
+                    {
+                        cover = serie.Cover;
+                    }
+
                     items.Add(new ChannelItemInfo()
                     {
                         ContentType = ChannelMediaContentType.Episode,
                         DateCreated = DateTimeOffset.FromUnixTimeSeconds(episode.Added).DateTime,
+                        Genres = GetGenres(serie.Genre),
                         Id = id,
-                        ImageUrl = episode.Info.MovieImage,
+                        ImageUrl = cover,
                         IsLiveStream = false,
                         MediaSources = sources,
                         MediaType = ChannelMediaType.Video,
                         Name = parsedName.Title,
+                        Overview = episode.Info.Plot,
+                        People = GetPeople(serie.Cast),
                         Tags = new List<string>(parsedName.Tags),
                         Type = ChannelItemType.Media,
                     });
