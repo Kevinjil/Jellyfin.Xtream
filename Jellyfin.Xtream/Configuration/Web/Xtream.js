37132076a470f1ecc26cf5dbc73310d4c77faf49
Kevin Jilissen <info@kevinjilissen.nl>
2022-06-29 13:47:10 +0200

    Move table creation methods to shared library.
    Enable re-use for VOD and Series.

--- Jellyfin.Xtream/Configuration/Web/Xtream.js
+++ Jellyfin.Xtream/Configuration/Web/Xtream.js
@@ -10,20 +10,180 @@ $(document).ready(() => {
   document.head.appendChild(style);
 });
 
-export default {
-  getTabs: () => {
-    const tabs = [
-      {
-        href: url('XtreamCredentials.html'),
-        name: 'Xtream Credentials'
-      },
-      {
-        href: url('XtreamLive.html'),
-        name: 'Live TV'
+const htmlExpand = document.createElement('span');
+htmlExpand.ariaHidden = true;
+htmlExpand.classList.add('material-icons', 'expand_more');
+
+const createItemRow = (item, state, update) => {
+  const tr = document.createElement('tr');
+  tr.dataset['itemId'] = item.Id;
+
+  let td = document.createElement('td');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = state;
+  checkbox.onchange = update;
+  td.appendChild(checkbox);
+  tr.appendChild(td);
+
+  td = document.createElement('td');
+  const label = document.createElement('label');
+  label.innerText = item.Name;
+  td.appendChild(label);
+  tr.appendChild(td);
+
+  td = document.createElement('td');
+  if (item.HasCatchup) {
+    td.title = `Catch-up supported for ${item.CatchupDuration} days.`;
+
+    let span = document.createElement('span');
+    span.innerText = item.CatchupDuration;
+    td.appendChild(span);
+
+    span = document.createElement('span');
+    span.ariaHidden = true;
+    span.classList.add('material-icons', 'timer');
+    td.appendChild(span);
+  }
+  tr.appendChild(td);
+
+  return tr;
+}
+
+const populateItemsTable = (wrapper, table, items) => {
+  for (let i = 0; i < items.length; ++i) {
+    const item = items[i];
+    const state = wrapper.live !== undefined && (wrapper.live.length === 0 || wrapper.live.includes(item.Id));
+    const row = createItemRow(item, state, (e) => {
+      let live = wrapper.live;
+      if (e.target.checked) {
+        live ??= [];
+        live.push(item.Id);
+        if (items.every(s => live.includes(s.Id))) {
+          live = [];
+        }
+      } else {
+        if (live.length === 0) {
+          live = items.map(s => s.Id);
+        }
+        live = live.filter(id => id != item.Id);
+        if (live.length === 0) {
+          live = undefined;
+        }
       }
-    ];
-    return tabs;
+      wrapper.live = live;
+    });
+    table.appendChild(row);
+  }
+}
+
+const setCheckboxState = (checkbox, live) => {
+  checkbox.indeterminate = live !== undefined && live.length > 0;
+  checkbox.checked = live !== undefined && live.length === 0;
+}
+
+const createCategoryRow = (wrapper, category, loadItems) => {
+  const tr = document.createElement('tr');
+  tr.dataset['categoryId'] = category.Id;
+
+  let td = document.createElement('td');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  setCheckboxState(checkbox, wrapper.live);
+  td.appendChild(checkbox);
+  tr.appendChild(td);
+
+  const _wrapper = {
+    get live() { return wrapper.live; },
+    set live(value) {
+      wrapper.live = value;
+      setCheckboxState(checkbox, wrapper.live);
+    },
+  }
+
+  td = document.createElement('td');
+  td.innerHTML = category.Name;
+  tr.appendChild(td);
+
+  td = document.createElement('td');
+  const expand = document.createElement('button');
+  expand.type = 'button';
+  expand.classList.add('paper-icon-button-light');
+  expand.appendChild(htmlExpand.cloneNode(true));
+  expand.onclick = (e) => {
+    e.preventDefault();
+    const originalClick = expand.onclick;
+
+    Dashboard.showLoadingMsg();
+    expand.firstElementChild.classList.replace('expand_more', 'expand_less');
+    const table = document.createElement('table');
+    loadItems(category.Id).then((items) => {
+      populateItemsTable(_wrapper, table, items);
+      Dashboard.hideLoadingMsg();
+    });
+    checkbox.onchange = () => {
+      if (checkbox.checked) {
+        wrapper.live = [];
+      } else {
+        wrapper.live = undefined;
+      }
+      table.querySelectorAll('input[type="checkbox"]').forEach((c) => c.checked = checkbox.checked);
+    };
+    td.appendChild(table);
+
+    expand.onclick = () => {
+      expand.onclick = originalClick;
+
+      Dashboard.showLoadingMsg();
+      expand.firstElementChild.classList.replace('expand_less', 'expand_more');
+      td.removeChild(table);
+      Dashboard.hideLoadingMsg();
+    };
+  };
+  td.appendChild(expand);
+  tr.appendChild(td);
+
+  return tr;
+};
+
+const populateCategoriesTable = (table, loadConfig, loadCategories, loadItems) => {
+  Dashboard.showLoadingMsg();
+  const fetchConfig = loadConfig();
+  const fetchCategories = loadCategories();
+
+  return Promise.all([fetchConfig, fetchCategories])
+    .then(([config, categories]) => {
+      const data = config;
+      for (let i = 0; i < categories.length; ++i) {
+        const category = categories[i];
+        const wrapper = {
+          get live() { return data[category.Id]; },
+          set live(value) {
+            data[category.Id] = value;
+          },
+        }
+        const elem = createCategoryRow(wrapper, category, loadItems);
+        table.appendChild(elem);
+      }
+      Dashboard.hideLoadingMsg();
+      return data;
+    });
+}
+
+const tabs = [
+  {
+    href: url('XtreamCredentials.html'),
+    name: 'Xtream Credentials'
   },
+  {
+    href: url('XtreamLive.html'),
+    name: 'Live TV'
+  }
+];
+
+export default {
+  getTabs: () => tabs,
+  populateCategoriesTable,
   PluginConfig: {
     UniqueId: '5d774c35-8567-46d3-a950-9bb8227a0c5d'
   }
--- Jellyfin.Xtream/Configuration/Web/XtreamLive.js
+++ Jellyfin.Xtream/Configuration/Web/XtreamLive.js
@@ -1,144 +1,4 @@
 export default function (view) {
-  const htmlExpand = document.createElement('span');
-  htmlExpand.ariaHidden = true;
-  htmlExpand.classList.add('material-icons', 'expand_more');
-
-  const createStreamRow = (item, state, update) => {
-    const tr = document.createElement('tr');
-    tr.dataset['itemId'] = item.Id;
-
-    let td = document.createElement('td');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = state;
-    checkbox.onchange = update;
-    td.appendChild(checkbox);
-    tr.appendChild(td);
-
-    td = document.createElement('td');
-    const label = document.createElement('label');
-    label.innerText = item.Name;
-    td.appendChild(label);
-    tr.appendChild(td);
-
-    td = document.createElement('td');
-    if (item.HasCatchup) {
-      td.title = `Catch-up supported for ${item.CatchupDuration} days.`;
-
-      let span = document.createElement('span');
-      span.innerText = item.CatchupDuration;
-      td.appendChild(span);
-
-      span = document.createElement('span');
-      span.ariaHidden = true;
-      span.classList.add('material-icons', 'timer');
-      td.appendChild(span);
-    }
-    tr.appendChild(td);
-
-    return tr;
-  }
-
-  const populateCategoryTable = (wrapper, table, items) => {
-    for (let i = 0; i < items.length; ++i) {
-      const item = items[i];
-      const state = wrapper.live !== undefined && (wrapper.live.length === 0 || wrapper.live.includes(item.Id));
-      const row = createStreamRow(item, state, (e) => {
-        let live = wrapper.live;
-        if (e.target.checked) {
-          live ??= [];
-          live.push(item.Id);
-          if (items.every(s => live.includes(s.Id))) {
-            live = [];
-          }
-        } else {
-          if (live.length === 0) {
-            live = items.map(s => s.Id);
-          }
-          live = live.filter(id => id != item.Id);
-          if (live.length === 0) {
-            live = undefined;
-          }
-        }
-        wrapper.live = live;
-      });
-      table.appendChild(row);
-    }
-  }
-
-  const setCheckboxState = (checkbox, live) => {
-    checkbox.indeterminate = live !== undefined && live.length > 0;
-    checkbox.checked = live !== undefined && live.length === 0;
-  }
-
-  const createRow = (wrapper, category) => {
-    const tr = document.createElement('tr');
-    tr.dataset['categoryId'] = category.Id;
-
-    let td = document.createElement('td');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    setCheckboxState(checkbox, wrapper.live);
-    td.appendChild(checkbox);
-    tr.appendChild(td);
-
-    const _wrapper = {
-      get live() { return wrapper.live; },
-      set live(value) {
-        wrapper.live = value;
-        setCheckboxState(checkbox, wrapper.live);
-      },
-    }
-
-    td = document.createElement('td');
-    td.innerHTML = category.Name;
-    tr.appendChild(td);
-
-    td = document.createElement('td');
-    const expand = document.createElement('button');
-    expand.type = 'button';
-    expand.classList.add('paper-icon-button-light');
-    expand.appendChild(htmlExpand.cloneNode(true));
-    expand.onclick = (e) => {
-      e.preventDefault();
-      const originalClick = expand.onclick;
-
-      Dashboard.showLoadingMsg();
-      expand.firstElementChild.classList.replace('expand_more', 'expand_less');
-      const table = document.createElement('table');
-      ApiClient.fetch({
-        url: ApiClient.getUrl(`Xtream/LiveCategories/${category.Id}`),
-        type: 'GET',
-      }).then((response) => response.json())
-        .then((items) => {
-          populateCategoryTable(_wrapper, table, items);
-          Dashboard.hideLoadingMsg();
-        });
-      checkbox.onchange = () => {
-        if (checkbox.checked) {
-          wrapper.live = [];
-        } else {
-          wrapper.live = undefined;
-        }
-        table.querySelectorAll('input[type="checkbox"]').forEach((c) => c.checked = checkbox.checked);
-      };
-      td.appendChild(table);
-
-      expand.onclick = () => {
-        expand.onclick = originalClick;
-
-        Dashboard.showLoadingMsg();
-        expand.firstElementChild.classList.replace('expand_less', 'expand_more');
-        td.removeChild(table);
-        Dashboard.hideLoadingMsg();
-      };
-    };
-    td.appendChild(expand);
-    tr.appendChild(td);
-
-    return tr;
-  };
-
   view.addEventListener("viewshow", () => import(
     ApiClient.getUrl("web/ConfigurationPage", {
       name: "Xtream.js",
@@ -148,44 +8,34 @@ export default function (view) {
     const pluginId = Xtream.PluginConfig.UniqueId;
     LibraryMenu.setTabs('Live TV', 1, Xtream.getTabs);
 
-    Dashboard.showLoadingMsg();
-    const fetchConfig = ApiClient.getPluginConfiguration(pluginId);
-    const fetchCategories = ApiClient.fetch({
-      url: ApiClient.getUrl('Xtream/LiveCategories'),
-      type: 'GET',
-    }).then((response) => response.json());
+    const table = view.querySelector('#LiveContent');
+    Xtream.populateCategoriesTable(
+      table,
+      () => ApiClient.getPluginConfiguration(pluginId).then((config) => config.LiveTv),
+      () => ApiClient.fetch({
+        dataType: 'json',
+        type: 'GET',
+        url: ApiClient.getUrl('Xtream/LiveCategories'),
+      }),
+      (categoryId) => ApiClient.fetch({
+        dataType: 'json',
+        type: 'GET',
+        url: ApiClient.getUrl(`Xtream/LiveCategories/${categoryId}`),
+      }),
+    ).then((data) => {
+      view.querySelector('#XtreamLiveForm').addEventListener('submit', (e) => {
+        Dashboard.showLoadingMsg();
 
-    let liveData;
-    Promise.all([fetchConfig, fetchCategories])
-      .then(([config, categories]) => {
-        liveData = config.LiveTv;
-        const live = view.querySelector('#LiveContent');
-        for (let i = 0; i < categories.length; ++i) {
-          const category = categories[i];
-          const wrapper = {
-            get live() { return liveData[category.Id]; },
-            set live(value) {
-              liveData[category.Id] = value;
-            },
-          }
-          const elem = createRow(wrapper, category);
-          live.appendChild(elem);
-        }
-        Dashboard.hideLoadingMsg();
-      });
-
-    view.querySelector('#XtreamLiveForm').addEventListener('submit', (e) => {
-      Dashboard.showLoadingMsg();
-
-      ApiClient.getPluginConfiguration(pluginId).then((config) => {
-        config.LiveTv = liveData;
-        ApiClient.updatePluginConfiguration(pluginId, config).then((result) => {
-          Dashboard.processPluginConfigurationUpdateResult(result);
+        ApiClient.getPluginConfiguration(pluginId).then((config) => {
+          config.LiveTv = data;
+          ApiClient.updatePluginConfiguration(pluginId, config).then((result) => {
+            Dashboard.processPluginConfigurationUpdateResult(result);
+          });
         });
-      });
 
-      e.preventDefault();
-      return false;
+        e.preventDefault();
+        return false;
+      });
     });
   }));
 }
