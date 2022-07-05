9f5b2b57d5add129d71a9abbb4c35d43db54a074
Kevin Jilissen <info@kevinjilissen.nl>
2022-07-05 19:52:35 +0200

    Add plugin configuration for overriding TV channel properties.

---
+++ Jellyfin.Xtream/Configuration/ChannelOverrides.cs
@@ -0,0 +1,45 @@
+// Copyright (C) 2022  Kevin Jilissen
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace Jellyfin.Xtream.Configuration
+{
+    /// <summary>
+    /// Override configuration for a Live TV channel.
+    /// </summary>
+    public class ChannelOverrides
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChannelOverrides"/> class.
+        /// </summary>
+        public ChannelOverrides()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the TV channel number.
+        /// </summary>
+        public int? Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TV channel name.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the url of the channel logo.
+        /// </summary>
+        public string? LogoUrl { get; set; }
+    }
+}
