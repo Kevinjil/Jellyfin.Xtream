24acce5ef505aa5c1700355c33723862ea59a833
Kevin Jilissen <info@kevinjilissen.nl>
2024-05-15 18:55:14 +0200

    Add registrator as it is needed in 10.9

---
+++ Jellyfin.Xtream/PluginServiceRegistrator.cs
@@ -0,0 +1,35 @@
+// Copyright (C) 2022  Kevin Jilissen
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Channels;
+using MediaBrowser.Controller.LiveTv;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Xtream;
+
+/// <inheritdoc />
+public class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    /// <inheritdoc />
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    {
+        serviceCollection.AddSingleton<ILiveTvService, LiveTvService>();
+        serviceCollection.AddSingleton<IChannel, CatchupChannel>();
+        serviceCollection.AddSingleton<IChannel, SeriesChannel>();
+        serviceCollection.AddSingleton<IChannel, VodChannel>();
+    }
+}
