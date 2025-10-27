e53f9a4412ae5a001a2ebddffee15c6cc373d8d9
Kevin Jilissen <info@kevinjilissen.nl>
2025-10-27 23:05:25 +0100

    Add status check on credentials page

--- Jellyfin.Xtream/Configuration/Web/XtreamCredentials.js
+++ Jellyfin.Xtream/Configuration/Web/XtreamCredentials.js
@@ -16,6 +16,35 @@ export default function (view) {
       Dashboard.hideLoadingMsg();
     });
 
+    const reloadStatus = () => {
+      const status = view.querySelector("#ProviderStatus");
+      const expiry = view.querySelector("#ProviderExpiry");
+      const cons = view.querySelector("#ProviderConnections");
+      const maxCons = view.querySelector("#ProviderMaxConnections");
+      const time = view.querySelector("#ProviderTime");
+      const timezone = view.querySelector("#ProviderTimezone");
+      const mpegTs = view.querySelector("#ProviderMpegTs");
+
+      Xtream.fetchJson('Xtream/TestProvider').then(response => {
+        status.innerText = response.Status;
+        expiry.innerText = response.ExpiryDate;
+        cons.innerText = response.ActiveConnections;
+        maxCons.innerText = response.MaxConnections;
+        time.innerText = response.ServerTime;
+        timezone.innerText = response.ServerTimezone;
+        mpegTs.innerText = response.SupportsMpegTs;
+      }).catch((_) => {
+        status.innerText = "Failed. Check server logs.";
+        expiry.innerText = "";
+        cons.innerText = "";
+        maxCons.innerText = "";
+        time.innerText = "";
+        timezone.innerText = "";
+        mpegTs.innerText = "";
+      });
+    };
+    reloadStatus();
+
     view.querySelector('#XtreamCredentialsForm').addEventListener('submit', (e) => {
       Dashboard.showLoadingMsg();
 
@@ -24,6 +53,7 @@ export default function (view) {
         config.Username = view.querySelector('#Username').value;
         config.Password = view.querySelector('#Password').value;
         ApiClient.updatePluginConfiguration(pluginId, config).then((result) => {
+          reloadStatus();
           Dashboard.processPluginConfigurationUpdateResult(result);
         });
       });
